1365e40dac00b2c72d8208bb4d9f5059c1728fc5
DouglasDuwe <douglas.duwe@hotmail.com>
2026-04-22 21:10:24 -0400

    Add files via upload

---
+++ exercicio_1.js
@@ -0,0 +1,10 @@
+let nome = "douglas";
+let idade = 28;
+let valorlogico = true;
+
+let mensagem1 = "Meu nome é " + nome + " e tenho " + idade + " anos." + valorlogico;
+
+console.log(typeof nome);      
+console.log(typeof idade);     
+console.log(typeof valorlogico); 
+console.log(mensagem1);
